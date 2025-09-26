aa1a33cae4d44b20f9f9427f39276f5c8f618a49
Arka-Pro <arkafebrian24@gmail.com>
2025-09-26 20:45:00 +0700

    Delete Tugas/soal3_input.py

--- Tugas/soal3_input.py
+++
@@ -1,4 +0,0 @@
-nama_depan = input("Masukkan Nama depan anda : ")
-nama_belakang = input("Masukkan Nama belakang anda : ")
-usia = int(input("Masukkan usia anda : "))
-print(f"Perkenalkan, saya {nama_depan} {nama_belakang}, saya berusia {usia} tahun.")
