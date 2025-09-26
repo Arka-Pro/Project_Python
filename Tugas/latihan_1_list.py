cb67839c9808a748010aa650665636729da11dc2
Arka-Pro <arkafebrian24@gmail.com>
2025-09-26 20:44:04 +0700

    Delete Tugas/latihan_1_list.py

--- Tugas/latihan_1_list.py
+++
@@ -1,9 +0,0 @@
-Hobi = ["Ngoding", "Sepedaan", "Boxing"]
-print("My hobi gw :", Hobi)
-print("Hobi pertama :", Hobi[0])
-print("Hobi kedua :", Hobi[1])
-print("Hobi ketiga :", Hobi[2])
-Hobi[1] = "Jogging"
-print("Perubahan Hobi :", Hobi)
-Hobi.append("Sepedaan")
-print("Penambahan Hobi :", Hobi)
