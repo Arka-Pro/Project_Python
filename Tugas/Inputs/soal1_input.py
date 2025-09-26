6b13dfff54740695301f1e812b5403d25c650392
Arka-Pro <arkafebrian24@gmail.com>
2025-09-26 20:42:24 +0700

    Add files via upload

---
+++ Tugas/Inputs/soal1_input.py
@@ -0,0 +1,4 @@
+nama = input("Masukkan Nama Lengkap anda : ")
+tahun_lahir = int(input("Masukkan Tahun lahir anda : "))
+umur = 2025 - tahun_lahir
+print(f"Halo, {nama}. Umurmu sekarang {umur} tahun.")
---
+++ Tugas/Inputs/soal2_input.py
@@ -0,0 +1,4 @@
+judul = input("Masukkan Judul buku yang anda inginkan : ")
+penulis = input("Sebutkan penulis buku tersebut : ")
+terbit = input("Kapan tahun terbitnya? : ")
+print(f"Buku {judul} ditulis oleh {penulis} dan diterbitkan pada tahun {terbit}.")
