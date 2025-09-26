e44798c81ea814987be59d3324d39961dcabcc22
Arka-Pro <arkafebrian24@gmail.com>
2025-09-26 20:45:09 +0700

    Delete Tugas/soal4_input.py

--- Tugas/soal4_input.py
+++
@@ -1,5 +0,0 @@
-print("Menghitung luas persegi panjang")
-panjang = int(input("Sebutkan angka panjang : "))
-lebar = int(input("Sebutkan angka lebar : "))
-luas = panjang * lebar
-print(f"Luas persegi panjang adalah {luas}")
