945487bd283b874f759f32e8b0ab0eec73eb4316
Arka-Pro <arkafebrian24@gmail.com>
2025-09-26 20:43:17 +0700

    Add files via upload

---
+++ Tugas/List dan Dictionary/latihan_2_dictionary.py
@@ -0,0 +1,7 @@
+Laptop = {"Merk": "HP-bs40xx", "CPU": "Intel i3 gen 6", "RAM_GB": "4"}
+print("My spek laptop gw :", Laptop)
+print("Spek CPU :", Laptop["CPU"])
+Laptop["Storage_GB"] = "946"
+print("Penambahan spek :", Laptop)
+Laptop["RAM_GB"] = "12"
+print("Perubahan RAM :", Laptop)
